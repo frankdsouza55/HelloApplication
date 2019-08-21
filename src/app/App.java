9aab94395265a7f0938d9d93f77d8284172761d9
Frank_Dsouza <frank_dsouza@persistent.co.in>
2019-08-21 14:34:40 +0530

    fixed one bug - line 6

--- src/app/App.java
+++ src/app/App.java
@@ -3,7 +3,7 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World");
-        System.out.println("learning GIT");
+        System.out.println("we are learning GIT");
         System.out.println("making commits");
     }
 }
