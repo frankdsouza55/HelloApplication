b4868f859edff05bac766be02f21359a1cf0adf7
Frank_Dsouza <frank_dsouza@persistent.co.in>
2019-08-21 14:28:39 +0530

    commit 3

--- src/app/App.java
+++ src/app/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World");
         System.out.println("learning GIT");
+        System.out.println("making commits");
     }
 }
