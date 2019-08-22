987ef6563c3a9dc4acdab73439e79a357d370a52
Frank D'Souza <frankdsouza55@gmail.com>
2019-08-22 11:23:52 +0530

    Removed few  lines

--- src/app/App.java
+++ src/app/App.java
@@ -7,8 +7,6 @@ public class App {
         System.out.println("making commits");
         System.out.println("i am not bored with the training");
 
-
-        System.out.println("added from master");
         System.out.println("added from new_branch");
     }
 }
